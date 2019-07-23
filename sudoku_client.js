123f7f5b57c3e39b5c4687f3cbf8d385b72a4c25
sven <sven@gwelt.net>
2019-07-23 23:58:50 +0200

    switched ws to socket.io

--- sudoku_client.js
+++ sudoku_client.js
@@ -1,10 +1,33 @@
 var current_sudoku_id=false;
 var current_sudoku=false;
 var current_pos=0;
-var keyset=[37,38,39,40,49,50,51,52,53,54,55,56,57,27];
+var keyset=[37,38,39,40,48,49,50,51,52,53,54,55,56,57,27];
 var tweets=[];
 init_grid();
-var ws=ws_open(location.origin.replace(/^http/, 'ws')+'/socket');
+
+var socket=false;
+var useSocketIO=true;
+
+function loadScript(src, done) {
+  var js = document.createElement('script');
+  js.src = src;
+  js.onload = function() {done(src+' executed')};
+  document.head.appendChild(js);
+}
+if (useSocketIO) {setTimeout(function(){SocketIO()},0)};
+
+function SocketIO() {if (!socket) {loadScript('/sudoku/socket.io/socket.io.js', function() {
+  socket = io.connect('', { 'path': '/sudoku/socket.io' });
+  socket.on('sudoku', function (msg) {log(msg)});
+  socket.on('message', function (msg) {log(msg)});
+  socket.on('disconnect', function (msg) {socket.close(); log('DISCONNECTED '+msg)});
+})}};
+
+function send_digit(digit) {send_message('put',{'pos':current_pos,'digit':digit})}
+function send_message(type,m) {
+  if (socket) {socket.emit(type||'message',m)} 
+  //else {var req = new XMLHttpRequest(); var hostname_port=location.hostname+":3000/255"; req.open('GET','http://'+hostname_port+'/m/'+m); req.send(); alert('Message delivered.')}
+}
 
 function tweet(t) {
   if (t) {tweets.push(t)}
@@ -19,8 +42,9 @@ function tweet(t) {
 function init_grid () {
   var grid=document.getElementById("grid");
   grid.style.textAlign='left';
-  var out="<style>body {text-align:left;font:22px 'Lucidia Console', Monaco, monospace}</style><div id=text></div><a href=javascript:select_current_sudoku(1)>1</a> <a href=javascript:select_current_sudoku(2)>2</a> <a href=javascript:select_current_sudoku(3)>3</a> <a href=javascript:select_current_sudoku(undefined)>INIT</a>";
-  grid.innerHTML=out; 
+  var out="<style>body {text-align:left;font:22px 'Lucidia Console', Monaco, monospace}</style><div id=text></div> <a href=javascript:send_message('init','')>INIT</a> <a href=javascript:send_message('reset','')>RESET</a>";
+  grid.innerHTML=out;
+  tweet(); 
 }
 
 document.onkeydown = function(event) {
@@ -30,6 +54,7 @@ document.onkeydown = function(event) {
       case 38: change_pos(-9); break; // UP
       case 39: change_pos(1); break; // RIGHT
       case 40: change_pos(9); break; // DOWN
+      case 48: send_digit(0); break; // 0 -
       case 49: send_digit(1); break; // 1
       case 50: send_digit(2); break; // 2
       case 51: send_digit(3); break; // 3
@@ -53,36 +78,6 @@ function change_pos(d) {
   if (d==-1 && current_pos%9==0) {new_pos=current_pos}
   if (new_pos<81 && new_pos>=0) {current_pos=new_pos}
   tweet();
-}
-
-function select_current_sudoku(id) {
-  current_sudoku=false;
-  current_sudoku_id=id;
-  send('{"id":"'+current_sudoku_id+'"}');
-}
-
-function send(msg) {ws.send(msg)}
-function send_digit(digit) {if (current_sudoku) {send('{"id":'+current_sudoku_id+',"pos":'+current_pos+',"digit":'+digit+'}')} else {log('SELECT A SUDOKU FIRST!')}}
-
-function ws_open(url) {
-  tweet('[CONNECTING WEBSOCKET-SERVER '+url+']');
-  try {ws=new WebSocket(url)} catch (err){alert(err);ws=false};
-  if (ws) {
-    ws.onopen = function () {select_current_sudoku(undefined)};
-    ws.onmessage = function (message) {
-      received_sudokus=JSON.parse(message.data);
-      var updated_current_sudoku = received_sudokus.find(s => s.id == current_sudoku_id);
-      if (updated_current_sudoku) {
-        current_sudoku=updated_current_sudoku; tweet('<b>#'+current_sudoku.id+':</b> '+current_sudoku.user_puzzle)
-        log('GOT UPDATE FOR CURRENT SUDOKU (#'+current_sudoku.id+')');
-      } 
-      else {
-        received_sudokus.forEach((s)=>{tweet('#'+s.id+': '+s.user_puzzle)})
-        log('GOT SUDOKU-UPDATES ('+received_sudokus.length+').');
-      };
-    }
-  }
-  return ws;
 }
 
 function log(m) {
--- sudoku_server.js
+++ sudoku_server.js
@@ -1,85 +1,57 @@
 'use strict';
-//var sudoku_solver = require('./sudoku_solver.js');
 var sudoku_generator = require('./sudoku_generator.js');
-const express = require('express');
-const SocketServer = require('ws').Server;
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server, {'path': '/sudoku/socket.io'});
+var bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3003;
-const INDEX = path.join(__dirname, 'index.html');
-const CLIENTJS = path.join(__dirname, 'sudoku_client.js');
-const server = express()
-  .use((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    if (req.url=="/") {res.sendFile(INDEX)}
-    if (req.url=="/sudoku_client.js") {res.sendFile(CLIENTJS)}
-    if (req.url=="/new") {res.send(new Sudoku().puzzle);}
-  })
-  .listen(PORT, function() {process.stdout.write(`\x1b[44m SUDOKU SERVER LISTENING ON PORT ${ PORT } \x1b[0m `)});
-const wss = new SocketServer({ server });
 
-///--- MAIN ---///
-var sudokus=[];
-var sudokuID=0;
+server.listen(PORT, function() {process.stdout.write(`\x1b[44m SUDOKU SERVER LISTENING ON PORT ${ PORT } \x1b[0m \n`)});
 
-///--- SUDOKU-CLASS ---///
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('(/sudoku)?/$', function(req,res) {res.sendFile(path.join(__dirname, 'index.html'))}); //res.setHeader('Access-Control-Allow-Origin','*');
+app.use('(/sudoku)?/sudoku_client.js', function(req,res) {res.sendFile(path.join(__dirname, 'sudoku_client.js'))});
+
 function Sudoku() {
-  //sudokus.pop();
-  sudokus.push(this);
-  this.id=++sudokuID;
-  var sudoku=sudoku_generator.generate();
-  this.puzzle=sudoku[0];
-  //this.solution=sudoku[1];
   this.init();
 }
 
 Sudoku.prototype.init = function () {
-  this.user_puzzle=this.puzzle;
+  var sudoku=sudoku_generator.generate();
+  this.puzzle=sudoku[0];
+  this.solution=sudoku[1];
+  this.reset();
+}
+
+Sudoku.prototype.reset = function () {
+  this.current=this.puzzle;
   broadcast(this);
 }
 
 Sudoku.prototype.set = function (pos,digit) {
-  var new_user_puzzle=this.user_puzzle.substr(0,pos)+digit+this.user_puzzle.substr(pos+1,81-pos-1);
-  this.user_puzzle=new_user_puzzle;
-  //log('SET #'+this.id+': '+pos+'='+digit);
-  broadcast(this);
+  if ( (81>pos>=0) && (/^[0-9]$/.test(digit)) ) {
+    if (this.puzzle[pos]=='-') {
+      if (digit==0) {digit='-'}
+      this.current=this.current.substr(0,pos)+digit+this.current.substr(pos+1,81-pos-1);
+      log('SET '+pos+'='+digit);
+      broadcast(this);
+    }
+  }
 }
 
-new Sudoku();
+var sudoku=new Sudoku();
 
-///--- CONNECTION-HANDLER ---///
-function broadcast(sudoku) {try{wss.clients.forEach((ws) => {
-  if (sudoku.id==ws.current_sudoku) {ws.send('['+JSON.stringify(sudoku)+']')}})
-} catch(err){};}
+function broadcast() {try{io.sockets.emit('sudoku',JSON.stringify(sudoku))} catch(err){};}
 
-wss.on('connection', (ws) => {
-  ws.current_sudoku=false;
-  ws.on('message', (msg) => {
-    var request='';
-    try {
-      request=JSON.parse(msg);
-      var sudoku=sudokus.find(s => s.id == request.id);
-      if (sudoku) {
-        // save as current_sudoku for this client
-        ws.current_sudoku=sudoku.id;
-        // set
-        request.pos=parseInt(request.pos);
-        request.digit=parseInt(request.digit);
-        if (!isNaN(request.pos) && !isNaN(request.digit) && (-1>request.pos<81) && (0>request.digit<10)) {
-          sudoku.set(request.pos,request.digit)
-        } 
-        else {
-          ws.send('['+JSON.stringify(sudoku)+']');
-        }
-      }
-      // send complete dataset
-      else {
-        //log('THERE IS NO SUDOKU WITH ID '+request.id+'. SENDING COMPLETE DATASET.');
-        ws.send(JSON.stringify(sudokus));
-      }
-    } catch (e) {
-      //log(e);
-    }
-  });
+io.on('connection', (socket) => {
+  socket.emit('sudoku',JSON.stringify(sudoku));
+  socket.on('put', (msg) => {sudoku.set(msg.pos,msg.digit)});
+  socket.on('init', (msg) => {sudoku.init()});
+  socket.on('reset', (msg) => {sudoku.reset()});
 });
 
 function log(m) {
