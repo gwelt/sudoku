f21d5cda8cde13fc012ab66c996044230a0b1aba
sven <sven@gwelt.net>
2019-07-24 20:24:14 +0200

    client divs starter

--- sudoku_client.js
+++ sudoku_client.js
@@ -1,9 +1,7 @@
-var current_sudoku_id=false;
-var current_sudoku=false;
 var current_pos=0;
+var current_sudoku='';
 var keyset=[37,38,39,40,48,49,50,51,52,53,54,55,56,57,27];
-var tweets=[];
-init_grid();
+init();
 
 var socket=false;
 var useSocketIO=true;
@@ -18,7 +16,7 @@ if (useSocketIO) {setTimeout(function(){SocketIO()},0)};
 
 function SocketIO() {if (!socket) {loadScript('/sudoku/socket.io/socket.io.js', function() {
   socket = io.connect('', { 'path': '/sudoku/socket.io' });
-  socket.on('sudoku', function (msg) {log(msg)});
+  socket.on('sudoku', function (msg) {log(msg); current_sudoku=JSON.parse(msg); update()});
   socket.on('message', function (msg) {log(msg)});
   socket.on('disconnect', function (msg) {socket.close(); log('DISCONNECTED '+msg)});
 })}};
@@ -26,25 +24,32 @@ function SocketIO() {if (!socket) {loadScript('/sudoku/socket.io/socket.io.js', 
 function send_digit(digit) {send_message('put',{'pos':current_pos,'digit':digit})}
 function send_message(type,m) {
   if (socket) {socket.emit(type||'message',m)} 
-  //else {var req = new XMLHttpRequest(); var hostname_port=location.hostname+":3000/255"; req.open('GET','http://'+hostname_port+'/m/'+m); req.send(); alert('Message delivered.')}
 }
 
-function tweet(t) {
-  if (t) {tweets.push(t)}
-  while (tweets.length>1) {tweets.splice(0,1)}
-  var etweet=document.getElementById('text');
-  if (etweet) {
-    etweet.innerHTML="current_pos="+current_pos+"<br>";
-    tweets.forEach(function(tt) {etweet.innerHTML+=tt+"<br>"});
-  };
-}
-
-function init_grid () {
-  var grid=document.getElementById("grid");
+function init() {
+  var grid=document.getElementById("sudoku");
   grid.style.textAlign='left';
   var out="<style>body {text-align:left;font:22px 'Lucidia Console', Monaco, monospace}</style><div id=text></div> <a href=javascript:send_message('init','')>INIT</a> <a href=javascript:send_message('reset','')>RESET</a>";
-  grid.innerHTML=out;
-  tweet(); 
+
+  var g='';
+  g+='<style>.cr {clear:both}</style>';
+  g+='<style>.tc {display:table-cell;vertical-align:middle}</style>';
+  g+='<style>.f {float:left;text-align:center;display:table;margin:0.1rem;background-color:#00ff00;width:50px;height:50px;}</style>';
+  g+='<div class=cr></div><div id=0 class=f>NULL</div><div id=1 class=f>EINS</div><div id=2 class=f>ZWEI</div>';
+
+  grid.innerHTML=out+g;
+  update(); 
+}
+
+function update() {
+  var i=0;
+  while (i<3) {
+  	var e=document.getElementById(i);
+  	e.style.background=(i==current_pos)?'#0444e0':'#f0f0f0';
+  	e.innerHTML=(current_sudoku.current)?(current_sudoku.current[i]>0?'<div class=tc>'+current_sudoku.current[i]+'</div>':''):'';
+		i++;
+  }
+  document.getElementById('text').innerHTML="current_pos="+current_pos+"<br>";
 }
 
 document.onkeydown = function(event) {
@@ -64,7 +69,7 @@ document.onkeydown = function(event) {
       case 55: send_digit(7); break; // 7
       case 56: send_digit(8); break; // 8
       case 57: send_digit(9); break; // 9
-      case 27: select_current_sudoku(undefined); break; // ESC (QUIT/START)
+      case 27: init(); break; // ESC (QUIT/START)
     }
     event.cancelBubble = true;
     event.returnValue = false;
@@ -77,7 +82,7 @@ function change_pos(d) {
   if (d==1 && current_pos%9==8) {new_pos=current_pos}
   if (d==-1 && current_pos%9==0) {new_pos=current_pos}
   if (new_pos<81 && new_pos>=0) {current_pos=new_pos}
-  tweet();
+  update();
 }
 
 function log(m) {
